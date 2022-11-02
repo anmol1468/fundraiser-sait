06590ec63acd47f1949be842bb3ab33fb734f36e
anmol1468 <69892413+anmol1468@users.noreply.github.com>
2022-11-01 20:08:01 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@
 import './App.css';
 
 function App() {
+  
+  window.onload = function() {
+    // similar behavior as an HTTP redirect
+    window.location.replace("https://www.eventbrite.com/e/dance-party-fundraiser-tickets-459533455937");
+}
+  
   return (
     <div className="App">
       I (Anmol) will make changes to this page and add new content.
