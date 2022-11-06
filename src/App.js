f61935e5b943629e220f52e6462a66f90f37fcf3
Anmol <anmolsingh1468@gmail.com>
2022-11-05 19:44:39 -0600

    okay

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import './App.css';
 
 function App() {
   
-  window.onload = function() {
-    // similar behavior as an HTTP redirect
-    window.location.replace("https://www.eventbrite.com/e/dance-party-fundraiser-tickets-459533455937");
-}
-  
   return (
     <div className="App">
-      redirecting
+      Okay
     </div>
   );
 }
