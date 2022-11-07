75d6e0d70cca1828beda2924d7b7fc25bc595c31
Anmol <anmolsingh1468@gmail.com>
2022-11-06 18:40:06 -0700

    info about sponser

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -5,7 +5,12 @@ function Info() {
   return (
     <div className='info'>
       <h3>More info about our event.</h3>
-      <p>We are a group of SAIT students. As a part of our project management class, we are organizing this fundraising event in the support of Calgary Food Bank. <br /> </p>
+      <p>We are a group of SAIT students. As a part of our project management class, we are organizing this fundraising event in the support of Calgary Food Bank. <br /> 
+      <br />
+      In our event you would get to attend a zumba class instructed by a talented group of people from Zumba with Aspire to Inspire.
+      <br /><br />
+      If you want to know more about Aspire to Inspire, you can visit their website by clicking <a target="_blank" href="https://www.atizumba.com/">here</a>
+      </p>
     </div>
   )
 }
